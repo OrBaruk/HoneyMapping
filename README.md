27c16cdaa6e145cf8aaa05247d0aec34b78f31a1
OrBaruk <orcansian@gmail.com>
2014-10-14 13:00:48 -0300

    minor updates to usage

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 --- Initial prototype implementation ---
+
+Requires:
+-django
+-python
+
+To run server for local access only
+python manage.py runserver 127.0.0.1:8000
--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import patterns, url
 from main import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^jvm/$', views.jvectormap, name='jvectormap'),    
+    url(r'^$', views.jvectormap, name='jvectormap'),    
 )
