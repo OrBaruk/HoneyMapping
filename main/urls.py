6e6bf0e3180ac3bf958ea52da31aed1eb9847a66
OrBaruk <orcansian@gmail.com>
2015-07-08 16:32:44 -0300

    Implements performance improvements from test

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,5 @@ from main import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),    
-	url(r'^teste/(\d{4})/(\d{1,2})/(\d{1,2})$', views.teste, name='index'),    
-	url(r'^report/(\d{4})/(\d{1,2})$', views.report_month, name='index'),    
-	url(r'^report/(\d{4})/(\d{1,2})/(\d{1,2})$', views.report_day, name='index'),    
+	url(r'^report/(\d{4})/(\d{2})/(\d{1,2})/(\d{1,2})/(\d{1,2})/(\d{4})/(\d{2})/(\d{1,2})/(\d{1,2})/(\d{1,2})/$', views.report, name='index'),    
 )
--- main/views.py
+++ main/views.py
@@ -14,93 +14,15 @@ import time
 def index(request):
 	return render(request, 'main/index.html')
 
-def report_month(request, year, month):
-	sources = Source.objects.all().filter(attack__dateTime__year=year, attack__dateTime__month=month)
-	
-	count   = dict()
-	markers = dict()
-	regions = dict()
+def report(request, startYear, startMonth, startDay, startHour, startMinute, endYear, endMonth, endDay, endHour, endMinute):	
+	#Undefined behaviour for invalid dates
 
-	i = 0
-	for s in sources:
-		l 		= s.location
-		cc		= l.countryCode
-		size 	= 1 # s.quantity	
-
-		if l.pk in markers:
-			count[l.pk] = count[l.pk] + size
-		else:
-			print(i)
-			i = i + 1
-		
-			m = dict()
-			m['name']  	= s.protocol
-			m['port']  	= s.port
-			m['ip']    	= l.pk
-			m['count'] 	= size
-			m['latLng']	= [str(l.latitude), str(l.longitude)]
-			m['city']  	= l.cityName
-			m['countryCode'] = cc
-			m['collector'] = s.collector
-			markers[l.pk] = m
-			count[l.pk] = size
-
-		if cc in regions:
-			regions[cc] += size
-		else:
-			regions[cc] = size
-
-	mapdata = dict()
-	mapdata['markers'] = list(markers.values())
-	mapdata['count'] = list(count.values())
-	mapdata['regions'] = regions
-
-	return render(request, 'main/teste.html')
-#	return HttpResponse(json.dumps(mapdata))
-
-def report_day(request, year, month, day):
-	sources = Source.objects.all().filter(attack__dateTime__year=year, attack__dateTime__month=month, attack__dateTime__day=day)
-	
-	count   = dict()
-	markers = dict()
-	regions = dict()
-
-	for s in sources.iterator():
-		l 		= s.location
-		cc		= l.countryCode
-		size 	= s.quantity	
-
-		if l.pk in markers:
-			count[l.pk] = count[l.pk] + size
-		else:
-			m = dict()
-			m['name']  	= s.protocol
-			m['port']  	= s.port
-			m['ip']    	= l.pk
-			m['count'] 	= size
-			m['latLng']	= [str(l.latitude), str(l.longitude)]
-			m['city']  	= l.cityName
-			m['countryCode'] = cc
-			m['collector'] = s.collector
-			markers[l.pk] = m
-			count[l.pk] = size
-
-		if cc in regions:
-			regions[cc] += size
-		else:
-			regions[cc] = size
-
-	mapdata = dict()
-	mapdata['markers'] = list(markers.values())
-	mapdata['count'] = list(count.values())
-	mapdata['regions'] = regions
-
-	return HttpResponse(json.dumps(mapdata))
-
-def teste(request, year, month, day):
 	start = time.time()	
-	startDate = year+'-'+month+'-'+day
-	endDate   = year+'-'+month+'-'+day+" 23:59:59"
+	startDate = startYear+'-'+startMonth+'-'+startDay+' '+startHour+':'+startMinute+':00'
+	endDate   = endYear+'-'+endMonth+'-'+endDay+' '+endMinute+':'+endHour+':59'
+
+	print(startDate)
+	print(endDate)
 
 	cursor = connection.cursor()
 	queryString = ( "SELECT DISTINCT "
@@ -167,7 +89,6 @@ def teste(request, year, month, day):
 	mapdata['count'] = list(count.values())
 	mapdata['regions'] = regions
 
-	#return render(request, 'main/teste.html')
 	end = time.time()
 	print(end - start)
 
