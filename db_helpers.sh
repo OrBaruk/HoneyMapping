dfc15f93e02293b6d5e60771c6881652391bbf33
OrBaruk <orcansian@gmail.com>
2015-08-28 10:44:35 -0300

    update parser and db_helper

--- db_helpers.sh
+++ db_helpers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #FSWatch folders usar supervise/daemontools
-fswatch -0 monitorado/ | xargs -0 -n 1 -I {} ls -la {} >> logs.txt
+fswatch -0 monitorado/ | xargs -0 -n 1 -I {} ls -la --time-style=long-iso {} >> logs.txt
 
 #CRON  parser.py
 python mananage.py parser logs.txt
--- main/management/commands/parser.py
+++ main/management/commands/parser.py
@@ -14,48 +14,27 @@ class Command(BaseCommand):
 		self.stdout.write(args[0])
 
 		#call function to parse the logs
-		parse_logs('/Users/or/LAS/HoneyMapping/data/logs_jul2015/'+args[0]+'.txt', args[0], '/usr/local/share/GeoIP/GeoIPCity.dat')
+		# geoIPLibpath = "/usr/local/share/GeoIP/GeoIPCity.dat"
+		parse_logs(+args[0]+'.txt', args[0], args[1])
 
 		return
 
 
 def parse_logs(filepath, collectorName, geoIPLibpath):
-	# geoIPLibpath = "/usr/local/share/GeoIP/GeoIPCity.dat"
-
 	gi = GeoIP.open(geoIPLibpath, GeoIP.GEOIP_STANDARD)
 	f = open(filepath, 'r')
-	
+
 	for line in f.readlines():
-		#print(line)
+		print(line)
 
-
-		rows = line.split()		
-		if len(rows) != 9:# Checks if the line has correct parameters
+		rows = line.split()
+		if len(rows) != 6:# Checks if the line has correct parameters
 			continue
-		#print(rows[8])
 
-		m = dict()
-		m['Jan'] = '01'
-		m['Feb'] = '02'
-		m['Mar'] = '03'
-		m['Apr'] = '04'
-		m['May'] = '05'
-		m['Jun'] = '06'
-		m['Jul'] = '07'
 
-		aux = rows[5].split('-')
-		year	  = '2015' #aux[0]		
-		month	  = m[rows[5]] #aux[1]
-		day  	  = rows[6] #aux[2]
-
-		#print(rows[5]+' '+rows[6])
-
-		time 	  = rows[7]
-
-		aux	 = rows[8].split('-')
+		aux	 = rows[7].split('-')
 		if len(aux) != 4: # Checks if the line has correct parameters
 			continue
-		print(year+'-'+month+'-'+day+"\t"+rows[8])
 
 		protc     = aux[0]
 		port	  = aux[1]
@@ -65,11 +44,9 @@ def parse_logs(filepath, collectorName, geoIPLibpath):
 		#Obtains geographic information from ip address
 		gir = gi.record_by_addr(ipAddress)
 		if gir:
-			#if gir['city']:
-			city =  str(gir['city']) # Converts the string from Latin-1 according to GeoIP documentation
-			#else:
-			#	city = 'Unknonw'
-	
+			# Converts the string from Latin-1 according to GeoIP documentation
+			city =  str(gir['city'])
+
 			lat = gir['latitude']
 			lon = gir['longitude']
 			cc = str(gir['country_code'])
@@ -80,11 +57,10 @@ def parse_logs(filepath, collectorName, geoIPLibpath):
 			lon = '0'
 			cc  = '??'
 
-
 		#Saves information into database
 		l = IpLocation(
 				ip       	= ipAddress,
-				latitude 	= lat, 
+				latitude 	= lat,
 				longitude	= lon,
 				cityName 	= city,
 				countryCode = cc
@@ -100,8 +76,7 @@ def parse_logs(filepath, collectorName, geoIPLibpath):
 		s.save()
 		a = Attack(
 				key		  = hashKey,
-				dateTime  = year+'-'+month+'-'+day+' '+time, #rows[5]+' '+rows[6],
-
+				dateTime  = rows[5]+' '+rows[6],
 				source    = s,
 			)
 		a.save()
