1bb2a0982f1cb7e5fdcf07897531e4c90c90453a
OrBaruk <orcansian@gmail.com>
2016-02-12 17:00:53 -0200

    update script

--- launch_monitor.sh
+++ launch_monitor.sh
@@ -3,6 +3,5 @@
 path="${1:-/dev/stdin}"
 name=$(basename "$path" "")
 echo $path
-
-nohup fswatch -0 --event Created $path | xargs -0 -n 1 -I {} ls -la --time-style=long-iso {} >> /home/baruque/fswatch_test/"cti_"$name &
+nohup fswatch -0 --event Created $path | xargs -0 -n 1 -I {} ls -la --time-style=long-iso {} >> home/baruque/monitor_tests/"$2"_$name &
 
--- monitor.sh
+++ monitor.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-nohup fswatch -r -0 --event Created /home/pakkun/honey/cti/var/dionaea/bistreams/ | xargs -0 -n 1 -I {} ls -la --time-style=long-iso {} >> /home/baruque/fswatch_test/"cti_first" &
-
-fswatch -0 --event Created /home/pakkun/honey/cti/var/dionaea/bistreams/ | xargs -0 -n 1 -I {} /home/baruque/HoneyMapping/launch_monitor.sh {}
-
-#TESTES
-#nohup fswatch -r -0 --event Created /home/baruque/tmp/ | xargs -0 -n 1 -I {} ls -la --time-style=long-iso {} >> /home/baruque/fswatch_test/"cti_first" &
-
-#fswatch -0 --event Created /home/baruque/tmp | xargs -0 -n 1 -I {} /home/baruque/HoneyMapping/launch_monitor.sh {}
-
+nohup fswatch -0 --event Created /home/pakkun/honey/cti/dionaea/bistreams/ | xargs -0 -n 1 -I {} /home/baruque/HoneyMapping/launch_monitor.sh {} cti &
+nohup fswatch -0 --event Created /home/pakkun/honey/inpe/dionaea/bistreams/ | xargs -0 -n 1 -I {} /home/baruque/HoneyMapping/launch_monitor.sh {} inpe &
+nohup fswatch -0 --event Created /home/pakkun/honey/facom/dionaea/bistreams/ | xargs -0 -n 1 -I {} /home/baruque/HoneyMapping/launch_monitor.sh {} facom &
+nohup fswatch -0 --event Created /home/pakkun/honey/unicamp/dionaea/bistreams/ | xargs -0 -n 1 -I {} /home/baruque/HoneyMapping/launch_monitor.sh {} unicamp &
