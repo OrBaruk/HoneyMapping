5ed99efb8b7fc5c5cc8b05768cddf45a8b0d4c6e
OrBaruk <orcansian@gmail.com>
2016-02-15 14:26:50 -0200

    minor fix

--- scripts/fswatch_launcher.sh
+++ scripts/fswatch_launcher.sh
@@ -2,6 +2,6 @@
 
 path="${1:-/dev/stdin}"
 name=$(basename "$path" "")
-nohup sh -c "fswatch -0 --event Created $path | xargs -0 -n 1 -I {} ls -la --time-style=long-iso {} >> /home/baruque/HoneyMapping/var/data/$2.txt" </dev/null >/dev/null>&1 &
+nohup sh -c "fswatch -0 --event Created $path | xargs -0 -n 1 -I {} ls -la --time-style=long-iso {} >> /home/baruque/HoneyMapping/var/data/$2.txt" </dev/null >/dev/null 2>&1 &
 echo $! >> /home/baruque/HoneyMapping/var/pid.txt
 
