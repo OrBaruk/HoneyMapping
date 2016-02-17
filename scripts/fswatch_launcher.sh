7eac294cff9824771887270d815510cb8320c386
OrBaruk <orcansian@gmail.com>
2016-02-17 10:08:54 -0200

    Fix scripts?

--- scripts/fswatch_launcher.sh
+++ scripts/fswatch_launcher.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 path="${1:-/dev/stdin}"
-name=$(basename "$path" "")
 nohup sh -c "fswatch -0 --event Created $path | xargs -0 -n 1 -I {} ls -la --time-style=long-iso {} >> /home/baruque/HoneyMapping/var/data/$2.txt" </dev/null >/dev/null 2>&1 &
 echo $! >> /home/baruque/HoneyMapping/var/pid.txt
 
